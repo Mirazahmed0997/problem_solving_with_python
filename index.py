508d1a1b4c40bf3eb1cefaa8fe1a8c6fb2fd2d2f
unknown <ahmedmiraz87@gmail.com>
2026-04-06 14:26:50 +0600

    addDocInFunction

--- index.py
+++ index.py
@@ -1,7 +1,17 @@
 def check(age):
+
+    """
+     Check the person is under age or not
+    """
+
+
     if age < 18:
         print(f'{age} is Under Age')
     else:
         print(f'{age} is Adult')
 
-check(14)
+
+age= int(input("Enter your age : "))
+check(age)
+
+help(check)
