32b7619488df82ae9fab128f8f9c160e8a4f438c
unknown <ahmedmiraz87@gmail.com>
2026-04-07 17:58:28 +0600

    generatorFunction

--- iterator_generator.py
+++ iterator_generator.py
@@ -10,3 +10,19 @@ print(next(s_iter))
 print(next(s_iter))
 print(next(s_iter))
 print(next(s_iter))
+
+
+# for i in s:
+#     print(i)
+
+
+list=[x for x in range(500)]
+
+def data_loader(chunk_size,list):
+    for i in range(0,len(list),chunk_size):
+        yield list[i:i+chunk_size]
+
+x= data_loader(5,list)
+print(next(x))
+print(next(x))
+print(next(x))
