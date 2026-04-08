496d1d6867fe693fbf4253bb6867057e7e9fd9e4
unknown <ahmedmiraz87@gmail.com>
2026-04-08 13:47:37 +0600

    AppendFile

---
+++ file_handing/file_append.py
@@ -0,0 +1,3 @@
+with open('sample_data_for_write.txt','a') as file:
+    file.write("I am apending a line \n")
+    file.write("And its working\n")
--- file_handing/file_handling.py
+++ file_handing/file_handling.py
@@ -1,13 +1,13 @@
-file= open("sample.txt","r")
+# file= open("sample.txt","r")
 
-    content = file.readlines()
+#     content = file.readlines()
 
-    print(content)
-    print(type(content))
+#     print(content)
+#     print(type(content))
 
-    file.close()
+#     file.close()
 
-    print(file.closed)
+    # print(file.closed)
 
 # -------------------------------------
 
@@ -15,15 +15,18 @@ file= open("sample.txt","r")
 with open("sample.txt") as file:
     content = file.readlines()
 
-    print(content)
-    print(type(content))
+    content = list(map(str.strip,content)) 
+    filter_content= filter(lambda x: len(x)>=1,content)
+
+    print(list(filter_content))
+    # print(type(content))
 
 
-    print(file.closed)
+    # print(file.closed)
 
 # --------------------------------------
 
 with open("sample.txt") as file:
     for line in file:
        l= line.strip() # to minimise the extra new line
-       print(l)
+    #    print(l)
