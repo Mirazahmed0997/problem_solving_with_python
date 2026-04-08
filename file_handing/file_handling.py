4b7e08de1d441404aed4523358d067fac0eed043
unknown <ahmedmiraz87@gmail.com>
2026-04-08 12:59:09 +0600

    writeFile

--- file_handing/file_handling.py
+++ file_handing/file_handling.py
@@ -1,6 +1,29 @@
 file= open("sample.txt","r")
 
-content = file.readlines()
+    content = file.readlines()
 
-print(content)
-print(type(content))
+    print(content)
+    print(type(content))
+
+    file.close()
+
+    print(file.closed)
+
+# -------------------------------------
+
+
+with open("sample.txt") as file:
+    content = file.readlines()
+
+    print(content)
+    print(type(content))
+
+
+    print(file.closed)
+
+# --------------------------------------
+
+with open("sample.txt") as file:
+    for line in file:
+       l= line.strip() # to minimise the extra new line
+       print(l)
---
+++ file_handing/file_write_handling.py
@@ -0,0 +1,9 @@
+with open('sample_data_for_write.txt',"w") as file:
+    file.write("Hello I am writting \n")
+    file.write("Miraz Ahmed")
+
+
+    
+    
+
+
