c91be0426d754eb0525dc9454b2edbcee3d9965f
Miraz Ahmed <ahmedmiraz87@gmail.com>
2026-04-10 12:10:33 +0600

    file_practice_problem4

--- file_problem3.py
+++ file_problem3.py
@@ -1,2 +1,4 @@
 with open('notes.txt','w') as file :
-    file.write("Hello, This is my first file")
+    file.write("Hello,")
+    file.write("This is my first file,")
+    file.write("I am working on it")
